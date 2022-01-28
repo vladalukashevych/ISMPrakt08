a0d7364473914c5301f8e3aba4450c07fb4701fd
Vlada Lukashevych <ipz211_lvv@student.ztu.edu.ua>
2022-01-28 13:26:20 +0200

    Implemented class PlaceOfWork

---
+++ Person/PlaceOfWork.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Person
+{
+    class PlaceOfWork
+    {
+        public string CompanyName { set; get; }
+
+        public Address Address; 
+        public string ContactNumber { set; get; }
+        public string CompanyWebsite { set; get; }
+
+        public PlaceOfWork(string companyName, Address address, string contactNumber, string companyWebsite)
+        {
+            CompanyName = companyName;
+            Address = new Address(address);
+            ContactNumber = contactNumber;
+            CompanyWebsite = companyWebsite;
+        }
+
+        public PlaceOfWork(string companyName, Address address, string contactNumber)
+        {
+            CompanyName = companyName;
+            Address = address;
+            ContactNumber = contactNumber;
+            CompanyWebsite = "none";
+        }
+
+        public PlaceOfWork(PlaceOfWork placeOfWork)
+        {
+            CompanyName = placeOfWork.CompanyName;
+            Address = placeOfWork.Address;
+            ContactNumber = placeOfWork.ContactNumber;
+            CompanyWebsite = placeOfWork.CompanyWebsite;
+        }
+
+        public string GetPlaceOfWorkInfo()
+        {
+            return $"Company name: {CompanyName}\nAddress: {Address.GetAddress()}\n Contact number: {ContactNumber}" +
+                $"Company website: {CompanyWebsite}";
+        }
+    }
+}
