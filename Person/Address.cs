0ce36cd353ae0794d3589b7c3511335973eedb61
Vlada Lukashevych <ipz211_lvv@student.ztu.edu.ua>
2022-01-28 13:32:26 +0200

    Made public class Address

--- Person/Address.cs
+++ Person/Address.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Person
 {
-    class Address
+    public class Address
     {
         public string Country { set; get;}
         public string City { set; get; }
